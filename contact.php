df64af514bf798a8ea6d03b1fa41f9477ebbc7b0
pinsourir <elodieannejourdain@gmail.com>
2026-03-02 16:05:03 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,23 +1,27 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     $nom = htmlspecialchars($_POST['nom']);
     $email = htmlspecialchars($_POST['email']);
     $presence = htmlspecialchars($_POST['presence']);
-    $message = htmlspecialchars($_POST['message']);
+    $nb = htmlspecialchars($_POST['nb_personnes']);
+    $infos = htmlspecialchars($_POST['infos']);
 
-    $to = "pinsourir@gmail.com"; // Remplace par ton email
+    $to = "pinsourir@gmail.com"; // METS TON EMAIL ICI
     $subject = "Confirmation de présence - Mariage";
+
     $body = "Nom: $nom\n";
     $body .= "Email: $email\n";
     $body .= "Présence: $presence\n";
-    $body .= "Message: $message\n";
+    $body .= "Nombre de personnes: $nb\n";
+    $body .= "Infos complémentaires: $infos\n";
 
-    $headers = "From: $email";
+    $headers = "From: mariage@tondomaine.com";
 
     if (mail($to, $subject, $body, $headers)) {
-        echo "<h2>Merci pour votre réponse !</h2>";
+        echo "<h2 style='text-align:center;margin-top:50px;'>Merci pour votre réponse !</h2>";
     } else {
-        echo "<h2>Erreur lors de l'envoi. Réessayez plus tard.</h2>";
+        echo "<h2 style='text-align:center;margin-top:50px;'>Erreur lors de l'envoi. Vérifiez que votre hébergement supporte la fonction mail().</h2>";
     }
 }
 ?>
